f31b20ad7792afda67edc39507250db446ba5c08
DiscRiskandBisque <joshquin@gmail.com>
2018-03-27 03:32:24 -0400

    Added AccountData, MetaMask warning to LoadingContainer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import AccountData from './AccountData.js'
 import ContractData from './ContractData.js'
 import ContractForm from './ContractForm.js'
 import LoadingContainer from './LoadingContainer.js'
 
 export {
+  AccountData,
   ContractData,
   ContractForm,
   LoadingContainer
