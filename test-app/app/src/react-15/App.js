d1b0d4a8989637e43f1a6a4b5c1b5f8ad33069ab
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 15:58:46 -0500

    Fix react 15 app not building

--- test-app/app/src/react-15/App.js
+++ test-app/app/src/react-15/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { DrizzleProvider } from "drizzle-react";
 import { LoadingContainer } from "drizzle-react-components";
 
-import "./App.css";
+import "../App.css";
 
 import drizzleOptions from "../drizzleOptions";
 import MyContainer from "./MyContainer";
