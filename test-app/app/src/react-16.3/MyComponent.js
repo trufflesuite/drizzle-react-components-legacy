6d2a35b4c9542e5903c88c6473f3001ded4b8802
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-14 16:08:30 -0500

    Add in TutorialToken and ComplexStorage sections to react16.3 test-app

--- test-app/app/src/react-16.3/MyComponent.js
+++ test-app/app/src/react-16.3/MyComponent.js
@@ -5,7 +5,7 @@ import logo from "../logo.png";
 
 const { AccountData, ContractData, ContractForm } = newContextComponents;
 
-export default ({ accounts }) => (
+export default () => (
   <DrizzleContext.Consumer>
     {drizzleContext => {
       const { drizzle, drizzleState, initialized } = drizzleContext;
@@ -13,6 +13,7 @@ export default ({ accounts }) => (
         return "Loading...";
       }
 
+      const { accounts } = drizzleState;
       return (
         <div className="App">
           <div>
@@ -37,6 +38,87 @@ export default ({ accounts }) => (
               <ContractData drizzle={drizzle} drizzleState={drizzleState} contract="SimpleStorage" method="storedData" />
             </p>
             <ContractForm drizzle={drizzle} drizzleState={drizzleState} contract="SimpleStorage" method="set" />
+          </div>
+
+          <div className="section">
+            <h2>TutorialToken</h2>
+            <p>
+              Here we have a form with custom, friendly labels. Also note the token
+              symbol will not display a loading indicator. We've suppressed it with
+              the <code>hideIndicator</code> prop because we know this variable is
+              constant.
+            </p>
+            <p>
+              <strong>Total Supply: </strong>
+              <ContractData
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                contract="TutorialToken"
+                method="totalSupply"
+                methodArgs={[{ from: accounts[0] }]}
+              />{" "}
+              <ContractData
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                contract="TutorialToken"
+                method="symbol"
+                hideIndicator
+              />
+            </p>
+            <p>
+              <strong>My Balance: </strong>
+              <ContractData
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                contract="TutorialToken"
+                method="balanceOf"
+                methodArgs={[accounts[0]]}
+              />
+            </p>
+            <h3>Send Tokens</h3>
+            <ContractForm
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="TutorialToken"
+              method="transfer"
+              labels={["To Address", "Amount to Send"]}
+            />
+          </div>
+
+          <div className="section">
+            <h2>ComplexStorage</h2>
+            <p>
+              Finally this contract shows data types with additional considerations.
+              Note in the code the strings below are converted from bytes to UTF-8
+              strings and the device data struct is iterated as a list.
+            </p>
+            <p>
+              <strong>String 1: </strong>
+              <ContractData
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                contract="ComplexStorage"
+                method="string1"
+                toUtf8
+              />
+            </p>
+            <p>
+              <strong>String 2: </strong>
+              <ContractData
+                drizzle={drizzle}
+                drizzleState={drizzleState}
+                contract="ComplexStorage"
+                method="string2"
+                toUtf8
+              />
+            </p>
+            <strong>Single Device Data: </strong>
+            <ContractData
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+              contract="ComplexStorage"
+              method="singleDD"
+            />
           </div>
         </div >
       );
